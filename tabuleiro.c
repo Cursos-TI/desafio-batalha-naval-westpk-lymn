c82211ce33cda4213401c2a8ef767236447ffaaf
west. <132110432+westpk-lymn@users.noreply.github.com>
2025-03-09 18:57:10 +0000

    tabuleiro

---
+++ tabuleiro.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define TAMANHO_TABULEIRO 10
+
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0; 
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]); // 
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+
+    inicializarTabuleiro(tabuleiro);
+
+    printf("Tabuleiro de Batalha Naval:\n");
+    exibirTabuleiro(tabuleiro);
+
+    return 0;
+}
